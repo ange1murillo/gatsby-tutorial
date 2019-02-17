2d7e2aa2b05fc4837d3ff9a82c0260b9ceb24694
amurillojr92 <amurillojr92@gmail.com>
2019-02-17 04:53:31 -0500

    graphql data

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,7 +13,7 @@ export default ({ children }) => (
         <h3 style={{ display: `inline` }}>MySweetSite</h3>
       </Link>
       <ul style={{ listStyle: `none`, float: `right` }}>
-        <ListLink to="/">Home</ListLink>
+        <ListLink to="/pandaindex/">Home</ListLink>
         <ListLink to="/about/">About</ListLink>
         <ListLink to="/contact/">Contact</ListLink>
       </ul>
